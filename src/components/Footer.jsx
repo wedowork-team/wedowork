edf9b03b8bfc3542dfa7407d796ec58e22154b08
Satyam Jha <jhaasatyamm@gmail.com>
2026-06-18 09:18:16 +0530

    Updating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
       
       <div className="flex items-center gap-6">
          <div className="flex items-center gap-2">
-           <img src="src/assets/logo.png" alt="Logo" className="w-8 h-8 object-contain" />
+           <img src="/Users/satyam/STUDY MATERIAL/web devlopment/wedowork/wedowork/src/assets/logo.png" alt="Logo" className="w-8 h-8 object-contain" />
            <span className="text-[9px] uppercase tracking-widest text-text-muted">© 2026 WeDoWork Agency</span>
          </div>
       </div>
